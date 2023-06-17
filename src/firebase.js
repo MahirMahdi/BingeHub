13231fd213522aa7e6f19d135c4156f77eefaa94
Mahir <mahdimahir45@gmail.com>
2023-06-17 07:22:27 +0600

    refactored index and firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { getAuth,GoogleAuthProvider,FacebookAuthProvider,TwitterAuthProvider} from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+} from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,5 +22,4 @@ export const db = getFirestore(app);
 export const auth = getAuth();
 export const provider = new GoogleAuthProvider();
 export const facebook_provider = new FacebookAuthProvider();
-export const twitter_provider = new TwitterAuthProvider();
 export default app;
--- src/index.js
+++ src/index.js
@@ -2,29 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
 import AuthProvider from "./contexts/AuthContext.js";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { theme } from "./utils/theme.js";
 import ThemeProvider from "@mui/material/styles/ThemeProvider.js";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import App from "./app.js";
+import Layout from "./components/Layout.js";
 
-function App() {
-  return (
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <AuthProvider>
         <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/searched/:search" element={<Search />} />
-            <Route path="/movies/:page" element={<Movies />} />
-            <Route path="/tvshows/:page" element={<TvShows />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/watchlist/:id" element={<WatchList />} />
-            <Route path="/detail/:type/:id/:user_id" element={<Detail />} />
-          </Routes>
+          <Layout>
+            <App />
+          </Layout>
         </Router>
       </AuthProvider>
     </ThemeProvider>
-  );
-}
-
-root.render(<App />);
+  </React.StrictMode>
+);
