18554bb2445a53221320f314087cae82cd60227c
Mahir <mahdimahir45@gmail.com>
2023-06-17 06:22:35 +0600

    switched to browser router

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './App.css';
-import Home from './pages/Home.js'
-import Search from './pages/Search.js'
-import Movies from './pages/Movies.js'
-import TvShows from './pages/TvShows.js';
-import Login from './pages/Login.js';
-import WatchList from './pages/WatchList.js';
-import Detail from './pages/Detail.js';
-import AuthProvider from './contexts/AuthContext.js';
-import { HashRouter as Router,Route,Routes} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import AuthProvider from "./contexts/AuthContext.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { theme } from "./utils/theme.js";
+import ThemeProvider from "@mui/material/styles/ThemeProvider.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function App(){
-
-    return(
-        <AuthProvider>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/searched/:search' element={<Search/>}/>
-                    <Route path='/movies/:page' element={<Movies/>}/>
-                    <Route path='/tvshows/:page' element={<TvShows/>}/>
-                    <Route path='/login' element={<Login/>}/>
-                    <Route path='/watchlist/:id' element={<WatchList/>}/>
-                    <Route path='/detail/:type/:id/:user_id' element={<Detail/>}/>
-                </Routes>
-            </Router>
-        </AuthProvider>
-    )
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/searched/:search" element={<Search />} />
+            <Route path="/movies/:page" element={<Movies />} />
+            <Route path="/tvshows/:page" element={<TvShows />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/watchlist/:id" element={<WatchList />} />
+            <Route path="/detail/:type/:id/:user_id" element={<Detail />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
+  );
 }
 
-root.render(<App />)
+root.render(<App />);
