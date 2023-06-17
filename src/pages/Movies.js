82b09d48a6a13a89e96dd1046c47fb071592aa1c
Mahir <mahdimahir45@gmail.com>
2023-06-17 07:18:35 +0600

    refactored movies and tvshows

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,11 +1,61 @@
-import Navbar from "../components/Navbar.js";
-import Movie from "../components/Movie.js";
+import React, { useState, useEffect } from "react";
+import { Box, Pagination, Grid } from "@mui/material";
+import Item from "../components/Card.js";
+import { useNavigate, useParams } from "react-router-dom";
 
-export default function Movies(){
-    return(
-        <div>
-            <Navbar/>
-            <Movie/>
-        </div>
-    )
-};
+export default function Movies() {
+  const navigate = useNavigate();
+  const params = useParams();
+  const page = params.page;
+  const [movies, setMovies] = useState([]);
+
+  async function getMovies() {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}&language=en-US&page=${page}`
+    );
+    const data = await response.json();
+    const result = data.results;
+    setMovies(result);
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  async function handleChange(e) {
+    const currentPage = e.target.innerText;
+    navigate("/movies/" + currentPage);
+    window.location.reload();
+  }
+
+  return (
+    <Box sx={{ mt: 10 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {movies.map((movie) => (
+          <Item
+            key={movie.id}
+            id={movie.id}
+            type={"movie"}
+            title={movie.title}
+            image={movie.poster_path}
+            overview={movie.overview}
+          />
+        ))}
+      </Grid>
+      <Pagination
+        count={10}
+        sx={{ color: "white" }}
+        page={parseInt(page)}
+        onClick={handleChange}
+        color={"primary"}
+        className="pagination"
+        hidePrevButton
+        hideNextButton
+      />
+    </Box>
+  );
+}
--- src/pages/TvShows.js
+++ src/pages/TvShows.js
@@ -1,11 +1,59 @@
-import Navbar from "../components/Navbar.js";
-import Shows from "../components/Shows.js";
+import React, { useState, useEffect } from "react";
+import { Box, Pagination, Grid } from "@mui/material";
+import Item from "../components/Card.js";
+import { useNavigate, useParams } from "react-router-dom";
 
-export default function TvShows(){
-    return(
-        <div>
-            <Navbar/>
-            <Shows/>
-        </div>
-    )
-};
+export default function TvShows() {
+  const navigate = useNavigate();
+  const params = useParams();
+  const page = params.page;
+  const [shows, setShows] = useState([]);
+
+  async function getShows() {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}&language=en-US&page=${page}`
+    );
+    const data = await response.json();
+    const result = data.results;
+    setShows(result);
+  }
+
+  async function handleChange(e) {
+    const currentPage = e.target.innerText;
+    navigate("/tvshows/" + currentPage);
+    window.location.reload();
+  }
+
+  useEffect(() => {
+    getShows();
+  }, []);
+  return (
+    <Box sx={{ mt: 10 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {shows.map((show) => (
+          <Item
+            key={show.id}
+            id={show.id}
+            type={"tv"}
+            title={show.name}
+            image={show.poster_path}
+            overview={show.overview}
+          />
+        ))}
+      </Grid>
+      <Pagination
+        count={10}
+        page={parseInt(page)}
+        onClick={handleChange}
+        color={"primary"}
+        className="pagination"
+        hidePrevButton
+        hideNextButton
+      />
+    </Box>
+  );
+}
